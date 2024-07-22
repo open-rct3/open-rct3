68208e9f9a5b64d3cd482a985dcf6a3e11f18997
Chance Snow <git@chancesnow.me>
2024-07-21 19:36:35 -0500

    Add OVL dumper utility project

---
+++ lib/ovl/dumper/source/app.d
@@ -0,0 +1,8 @@
+/// License: GPL 2.0
+import std.stdio;
+
+import ovl.dumper;
+
+void main() {
+  writeln("Edit source/app.d to start your project.");
+}
---
+++ lib/ovl/dumper/source/package.d
@@ -0,0 +1,4 @@
+/// License: GPL 2.0
+module ovl.dumper;
+
+import ovl;
