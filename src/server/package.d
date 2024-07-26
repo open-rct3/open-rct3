95e4387443daed53363a8719ba4475e8eb9bb8bc
Chance Snow <git@chancesnow.me>
2024-07-26 01:51:27 -0500

    Extract API into its own module

---
+++ src/server/package.d
@@ -0,0 +1,40 @@
+/// License: AGPL 3.0
+module rct3.server;
+
+import std.stdio;
+import std.typecons : Tuple, tuple;
+import vibe.d;
+
+package shared auto running = false;
+package Tuple!(HTTPServerSettings, "settings", URLRouter, "router") server;
+
+shared static this() {
+  import rct3.server.routes : connectWebSocket, router, Server;
+
+  // TODO: Read server PORT and other configs from .env
+  const port = 8080;
+
+  void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error) {
+    res.render!("error.dt", req, error);
+  }
+
+  auto settings = new HTTPServerSettings;
+  settings.port = port;
+  settings.bindAddresses = ["::1", "127.0.0.1"];
+  settings.errorPageHandler = toDelegate(&errorPage);
+
+  server = tuple!("settings", "router")(settings, router);
+}
+
+void main() {
+  if (!finalizeCommandLineOptions()) return;
+
+  // Start the server and run the event loop
+  auto listener = listenHTTP(server.settings, server.router);
+  running = true;
+  runEventLoop();
+
+  // Server has exited
+  listener.stopListening();
+  running = false;
+}
--- src/server/routes.d
+++ src/server/routes.d
@@ -1,44 +1,46 @@
-/// License: AGPL 3.0
-module rct3.server;
+module rct3.server.routes;
 
 import std.conv : to;
-import std.stdio;
 import std.traits : Unconst, Unqual;
-import std.typecons : Tuple, tuple;
-import vibe.d;
+import vibe.core.log;
+import vibe.http.router;
+import vibe.http.server;
+import vibe.http.websockets;
+import vibe.web.web;
 
-package shared auto running = false;
-package Tuple!(HTTPServerSettings, "settings", URLRouter, "router") server;
-// See https://wiki.dlang.org/User:Csmith1991/Vibe.d_Documentation/websocket
-package WebSocket[] sockets;
-
-shared static this() {
+auto router() {
   import std.array : array;
   import std.path : asAbsolutePath, dirSeparator, expandTilde, isRooted;
   import std.file : exists;
+  import vibe.http.fileserver : serveStaticFiles;
 
   // TODO: Read public folder path, server PORT and other configs from .env
   const publicDir = "public/".expandTilde.asAbsolutePath.array.to!string;
-  const port = 8080;
 
   /// Use `/ws` to identify WebSocket requests, otherwise, serve files out of the public folder.
   auto router = new URLRouter;
   /// Section: Routes
   {
-    router.get("/", &index);
+    router.registerWebInterface(new Server);
     // WebSocket entry point
     router.get("/ws", handleWebSockets(&connectWebSocket));
     if (publicDir.isRooted && publicDir.exists)
       router.get("*", serveStaticFiles(publicDir));
   }
-
-  auto settings = new HTTPServerSettings;
-  settings.port = port;
-  settings.bindAddresses = ["::1", "127.0.0.1"];
-  settings.errorPageHandler = toDelegate(&errorPage);
-
-  server = tuple!("settings", "router")(settings, router);
+  return router;
 }
+
+class Server {
+  private SessionVar!(bool, "authenticated") ms_authenticated;
+
+  // GET /
+  void index() {
+    render!("index.dt");
+  }
+}
+
+// See https://wiki.dlang.org/User:Csmith1991/Vibe.d_Documentation/websocket
+package WebSocket[] sockets;
 
 /// Returns: Mutable reference to the given `object`.
 Unconst!T mut(T)(T object) if (is(Unqual!T : Object)) {
@@ -46,31 +48,9 @@ Unconst!T mut(T)(T object) if (is(Unqual!T : Object)) {
   return castFrom!T.to!(Unconst!T)(object);
 }
 
-void main() {
-  if (!finalizeCommandLineOptions()) return;
-
-  // Start the server and run the event loop
-  auto listener = listenHTTP(server.settings, server.router);
-  running = true;
-  runEventLoop();
-
-  // Server has exited
-  listener.stopListening();
-  running = false;
-}
-
-package:
-
-void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error) {
-  res.render!("error.dt", req, error);
-}
-
-void index(HTTPServerRequest req, HTTPServerResponse res) {
-  res.render!("index.dt", req);
-}
-
 void connectWebSocket(scope WebSocket socket) {
   import std.algorithm : canFind, countUntil, remove;
+  import vibe.d : seconds;
 
   // Note: *No* other headers are assignable in standard WebSocket clients.
   // See https://stackoverflow.com/a/4361358
