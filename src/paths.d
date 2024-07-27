a898a9d4201f37ac532434d8c726e4a9b97d0a8d
Chance Snow <git@chancesnow.me>
2024-07-26 20:56:44 -0500

    Search for RCT3 assets given known paths

--- src/paths.d
+++ src/paths.d
@@ -6,7 +6,7 @@ module rct3.paths;
 ///  $(LI $(ANCHOR https://steamdb.info/app/1368820 "RCT3 Complete Edition") (SteamDB))
 ///  $(LI $(ANCHOR https://www.protondb.com/app/1368820 "RCT3 Complete Edition on Steam Play") (ProtonDB))
 /// )
-struct Data {
+struct Paths {
   /// Installation data location.
   version (DDoc) static const string[] installData = [];
   else version (Windows) static const auto installData = [
@@ -15,7 +15,9 @@ struct Data {
   else version (OSX) static const auto installData = [
     "/Applications/RollerCoaster Tycoon 3 Platinum.app/Contents/Assets"
   ];
-  else version (Linux) static const auto installData = [];
+  else version (Linux) static const auto installData = [
+    // FIXME: What is the path to game OVLs with Proton for Linux?
+  ];
 
   ///
   version(DDoc) static const string[] exe = [];
@@ -23,51 +25,136 @@ struct Data {
     "C:\\Program Files\\RollerCoaster Tycoon 3 Platinum\\RCT3.exe"
   ];
   else version(OSX) static const auto exe = [
+    "/Applications/RollerCoaster Tycoon 3 Platinum.app/Contents/MacOS/RCT3",
     "/Applications/RollerCoaster Tycoon 3 Platinum.app/Contents/MacOS/RCT3_Exe"
   ];
-  else version (Linux) static const auto exe = [];
+  else version (Linux) static const auto exe = [
+    // FIXME: What is the path to RCT3.exe with Proton for Linux?
+  ];
 
   /// Configuration data location.
   version (DDoc) static const string[] appData = [];
   else version (Windows) static const auto appData = [
-    "%APPDATA%\\Atari\\RCT3",
-    "%APPDATA%\\Frontier\\RCT3"
+    "$APPDATA\\Atari\\RCT3",
+    "$APPDATA\\Frontier\\RCT3"
   ];
   else version (OSX) static const auto appData = [
     // FIXME: This path may vary based on version
-    "$HOME/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Platinum/AppData",
+    "~/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Platinum/AppData",
     // Contains Options.txt, MusicGenre.txt, and user save games, coasters, parks, structures, etc.
-    "$HOME/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Complete Edition"
+    "~/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Complete Edition"
   ];
   else version (Linux) static const auto appData = [
     // Steam Play (Proton on Linux) See https://www.protondb.com/app/1368820
     // FIXME: There may be differing IDs for other versions
-    "$STEAM_IBRARY/steamapps/compatdata/1368820/pfx"
+    "$STEAM_LIBRARY/steamapps/compatdata/1368820/pfx"
   ];
 
-  /// Saved game data location.
+  /// Saved game data location, including coasters, parks, structures, etc.
   version (DDoc) static const string[] userData = [];
   else version (Windows) static const auto userData = [
-    "%USERPROFILE%\\Documents\\RCT3"
+    "$USERPROFILE\\Documents\\RCT3"
   ];
   else version (OSX) static const auto userData = [
     // FIXME: This path may vary based on version
-    "$HOME/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Platinum",
-    // Contains Options.txt and (user?) save games, coasters, parks, structures, etc.
-    "$HOME/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Complete Edition"
+    "~/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Platinum",
+    "~/Library/Containers/com.aspyr.rct3.appstore/Data/Library/Application Support/RollerCoaster Tycoon 3 Complete Edition"
   ];
   else version (Linux) static const auto userData = [
     // Steam Play (Proton on Linux) See https://www.protondb.com/app/1368820
     // FIXME: There may be differing IDs for other versions
-    "$STEAM_IBRARY/steamapps/compatdata/1368820/pfx"
+    "$STEAM_LIBRARY/steamapps/compatdata/1368820/pfx"
   ];
+}
+
+/// Thrown when RCT3 game data could not be found.
+class GameNotFoundException : Exception {
+  import std.exception : basicExceptionCtors;
+
+  ///
+  mixin basicExceptionCtors;
+}
+
+///
+struct Data {
+  import std.algorithm : any, find, map;
+  import std.exception : enforce;
+  import std.file : dirEntries, exists, SpanMode;
+  import std.path : expandTilde;
+
+  /// Throws: When RCT3 installation data could not be found.
+  static auto installData(string glob = null) @property {
+    auto paths = Paths.installData.map!(path => path.expandTilde.expandEnvironmentVars);
+    enum notFoundMsg = "Could not find RCT3 installation data.";
+    enforce!GameNotFoundException(paths.any!exists, notFoundMsg);
+
+    foreach (path; paths) if (path.exists)
+      return path.dirEntries(glob is null ? "*" : glob, SpanMode.depth);
+    throw new GameNotFoundException(notFoundMsg);
+  }
+
+  /// Throws: When RCT3's executable could not be found.
+  static auto exe() @property {
+    import std.algorithm : filter;
+    import std.file : attrIsFile, getAttributes;
+
+    auto paths = Paths.exe.map!(path => path.expandTilde.expandEnvironmentVars);
+    enum notFoundMsg = "Could not find RCT3 executable.";
+    enforce!GameNotFoundException(paths.any!exists, notFoundMsg);
+
+    foreach (path; paths) if (path.exists) return path;
+    throw new GameNotFoundException(notFoundMsg);
+  }
+
+  /// Throws: When application data, e.g. `Options.txt`, could not be found.
+  static auto appData(string glob = null) @property {
+    auto paths = Paths.appData.map!(path => path.expandTilde.expandEnvironmentVars);
+    enum notFoundMsg = "Could not find RCT3 application data.";
+    enforce!GameNotFoundException(paths.any!exists, notFoundMsg);
+
+    foreach (path; paths) if (path.exists)
+      return path.dirEntries(glob is null ? "*" : glob, SpanMode.depth);
+    throw new GameNotFoundException(notFoundMsg);
+  }
+
+  /// Throws: When user data, e.g. parks and structures, could not be found.
+  static auto userData(string glob = null) @property {
+    auto paths = Paths.userData.map!(path => path.expandTilde.expandEnvironmentVars);
+    enum notFoundMsg = "Could not find RCT3 user data.";
+    enforce!GameNotFoundException(paths.any!exists, notFoundMsg);
+
+    foreach (path; paths) if (path.exists)
+      return path.dirEntries(glob is null ? "*" : glob, SpanMode.depth);
+    throw new GameNotFoundException(notFoundMsg);
+  }
 }
 
 unittest {
   import std.algorithm : equal;
 
-  assert(Data.installData.length);
-  assert(Data.exe.length);
-  assert(Data.appData.length);
-  assert(Data.userData.length);
+  assert(Paths.installData.length);
+  assert(Paths.exe.length);
+  assert(Paths.appData.length);
+  assert(Paths.userData.length);
+}
+
+version (FindRCT3) unittest {
+  import std.exception : assertNotThrown;
+
+  assertNotThrown(Data.installData);
+  assertNotThrown(Data.exe);
+  assertNotThrown(Data.appData);
+  assertNotThrown(Data.userData);
+}
+
+private string[string] env;
+
+private string expandEnvironmentVars(string value) {
+  import std.algorithm : canFind;
+  import std.process : environment;
+  import std.string : replace;
+
+  auto vars = env.length == 0 ? (env = environment.toAA) : env;
+  foreach (var; vars.keys) if (value.canFind("$" ~ var)) value = value.replace("$" ~ var, vars[var]);
+  return value;
 }
