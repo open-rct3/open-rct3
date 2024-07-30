3fc480289150fe44a41fc5e44e038114cf313e1f
Chance Snow <git@chancesnow.me>
2024-07-29 19:34:27 -0500

    Use a file picker to let the user select an OVL to dump

--- lib/ovl/dumper/src/app.d
+++ lib/ovl/dumper/src/app.d
@@ -1,6 +1,7 @@
 /// License: GPL 2.0
 import std.stdio;
 
+import nfde;
 import ovl;
 
 void main(string[] args) {
@@ -8,11 +9,15 @@ void main(string[] args) {
 
   string inputFile() {
     if (args.length > 1) return args[1];
+    // TODO: Show an open dialog only if a tty isn't connected
+    string path;
+    if (openDialog(path, [FilterItem("OVL archive files"c.ptr, "*.ovl"c.ptr)]) == Result.okay)
+      return path;
     write("OVL archive path: ");
     return stdin.readln;
   }
 
   auto file = inputFile;
-  format!"Reading %s"(file);
+  format!"Reading %s"(file).writeln;
   auto archive = Ovl.load(file);
 }
