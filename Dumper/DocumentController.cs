beb6cbfed7776170f96bcc7a9bdbdaa7f0707ece
Chance Snow <git@chancesnow.me>
2024-10-20 02:10:38 -0500

    Modernize OVL file type specifier

--- Dumper/DocumentController.cs
+++ Dumper/DocumentController.cs
@@ -8,11 +8,15 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 
 using ObjCRuntime;
+using UniformTypeIdentifiers;
 
 namespace Dumper;
 
 // See https://developer.apple.com/documentation/appkit/nsdocumentcontroller
 public class DocumentController : NSDocumentController {
+  [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "This app requires at least macOS 10.15")]
+  private readonly UTType ovlContentType = UTType.CreateFromIdentifier("com.open-rct3.ovl")!;
+
   public DocumentController() : base(NSObjectFlag.Empty) { }
   public DocumentController(NativeHandle handle) : base(handle) { }
 
@@ -27,14 +31,16 @@ public class DocumentController : NSDocumentController {
   // See https://learn.microsoft.com/en-us/dotnet/api/appkit.nsdocumentcontroller.opendocument?view=xamarin-mac-sdk-14
   // See https://stackoverflow.com/a/29709860/1363247
   [SuppressMessage("Interoperability", "CA1422:Validate platform compatibility", Justification = "This app requires at least macOS 10.15")]
+  [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "This app requires at least macOS 10.15")]
   public override void OpenDocument(NSObject? sender) {
     var dialog = NSOpenPanel.OpenPanel;
     var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
     dialog.DirectoryUrl = new NSUrl(home);
-    dialog.AllowsMultipleSelection = false;
-    //dialog.TreatsFilePackagesAsDirectories = true;
     dialog.CanCreateDirectories = false;
+    dialog.AllowsMultipleSelection = false;
+    dialog.AllowedContentTypes = [ovlContentType];
+    //dialog.TreatsFilePackagesAsDirectories = true;
     dialog.ReleaseWhenClosed();
     dialog.BeginCriticalSheet(NSApplication.SharedApplication.MainWindow, result => {
       if (result != 1) return;
