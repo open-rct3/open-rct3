9bd9b5c91414c8bf61e07a1b6efb68c7146f614a
Chance Snow <git@chancesnow.me>
2024-11-06 11:49:14 -0600

    Terminate app when last window is closed

--- OpenRCT3/Platforms/macOS/AppDelegate.cs
+++ OpenRCT3/Platforms/macOS/AppDelegate.cs
@@ -11,11 +11,20 @@ namespace OpenRCT3.Platforms.macOS;
 public class AppDelegate : NSApplicationDelegate {
   public static AppDelegate? Instance => NSApplication.SharedApplication.Delegate as AppDelegate;
 
+  [System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Interoperability",
+    "CA1422:Validate platform compatibility",
+    Justification = "This app requires at least macOS 10.15"
+  )]
   public override void DidFinishLaunching(NSNotification notification) {
     NSApplication.SharedApplication.ActivateIgnoringOtherApps(true);
   }
 
   public override void WillTerminate(NSNotification notification) {
     // TODO: Tear down WebGPU and other unmanaged resources
+  }
+
+  public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) {
+    return true;
   }
 }
