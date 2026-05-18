31130699117c29001733149dca2531f5b3963604
Chance Snow <git@chancesnow.me>
2026-05-18 07:00:32 -0500

    Add a Progress model to measure world loading status

--- OpenRCT3/Simulation/World.cs
+++ OpenRCT3/Simulation/World.cs
@@ -4,6 +4,7 @@
 //   - Chance Snow <git@chancesnow.me>
 //
 // Copyright © 2026 OpenRCT3 Contributors. All rights reserved.
+using OpenRCT3.Streaming;
 
 namespace OpenRCT3.Simulation;
 
@@ -11,13 +12,19 @@ namespace OpenRCT3.Simulation;
 /// Represents the game world including the current park, terrain, objects, and people.
 /// </summary>
 public class World {
+  private Progress progress = Progress.COMPLETE;
+
+  /// <summary>
+  /// The current progress of the world loading.
+  /// </summary>
+  public Progress Progress => progress;
   public Terrain? Terrain { get; private set; }
   public Park? Park { get; private set; }
 
-  public void Load() {
-    Park = new Park();
-    Terrain = Terrain.Load();
-  }
+  public void Load() => progress = Progress.MeasureTasks([
+      new(() => Park = new Park(), "Loading park"),
+      new(() => Terrain = Terrain.Load(), "Loading terrain"),
+    ]).Progress;
 
   // TODO: Use Task.Factory.StartNew to do slow async work in the background
   // TODO: Implement IDisposable
