5bb7b4ace9ff7210d26c50eec34200206660702b
Chance Snow <git@chancesnow.me>
2026-05-27 17:10:32 -0500

    Fix OpenGL state management logic

--- OpenCobra/GDK/GLState.cs
+++ OpenCobra/GDK/GLState.cs
@@ -13,18 +13,39 @@ using Silk.NET.OpenGL;
 namespace OpenCobra.GDK;
 
 /// <summary>
+/// <para>
 /// Captures and restores the OpenGL state machine via a thread-safe concurrent
 /// stack. Avoids manual save/restore boilerplate across the render loop and
-/// ImGui integration.
+/// <see href="https://www.nuget.org/packages/Silk.NET.OpenGL.Extensions.ImGui">ImGui</see>
+/// integration.
+/// </para>
 /// </summary>
-/// <remarks>
-/// Use with <c>using var guard = GLState.Push();</c> for RAII-style automatic
-/// restoration.
+/// <remarks class="usage">
+/// <para>
+/// <c>using var guard = GLState.Push();</c> for
+/// <abbr title="Resource acquisition is initialization">RAII</abbr>-style automatic
+/// restoration. When <c>guard</c> is disposed, <see cref="Pop"/> is automatically called.
+/// </para>
+/// </remarks>
+/// <remarks class="notes">
+/// <para>
+/// <see cref="PolygonMode"/> is always restored using
+/// <see cref="TriangleFace.FrontAndBack"/> regardless of the current profile.
+/// </para>
+/// <para>
+/// This is intentional: <c>GL_FRONT_AND_BACK</c> is valid in all OpenGL versions
+/// (2.0+), whereas <c>GL_FRONT</c> and <c>GL_BACK</c> are invalid in core
+/// profile (OpenGL 3.2+). See
+/// <see href="https://docs.gl/gl2/glPolygonMode">glPolygonMode</see>
+/// and <see href="https://docs.gl/gl3/glPolygonMode">glPolygonMode (GL3)</see>.
+/// </para>
 /// </remarks>
 // ReSharper disable once InconsistentNaming
 public readonly struct GLState : IDisposable {
   private readonly static ConcurrentStack<GLState> stack = new();
 
+  private readonly Version version;
+  private readonly ContextFlagMask contextFlags;
   private readonly int activeTexture;
   private readonly int currentProgram;
   private readonly int textureBinding2D;
@@ -45,10 +66,19 @@ public readonly struct GLState : IDisposable {
   private readonly bool depthTestEnabled;
   private readonly bool stencilTestEnabled;
   private readonly bool scissorTestEnabled;
-  private readonly int frontPolygonMode;
-  private readonly int backPolygonMode;
+  private readonly int frontAndBackPolygonMode;
+
+  // FIXME: This feels incorrect. See 
+  public readonly bool IsCoreProfile =>
+    contextFlags.HasFlag(ContextFlagMask.DebugBit) ||
+    version.Major >= 3;
 
   private GLState(GL gl) {
+    if (Version.TryParse(gl.GetStringS(StringName.Version), out var version))
+      this.version = version;
+    else this.version = new();
+    contextFlags = (ContextFlagMask)gl.GetInteger(GetPName.ContextFlags);
+
     activeTexture = gl.GetInteger(GLEnum.ActiveTexture);
     currentProgram = gl.GetInteger(GLEnum.CurrentProgram);
     textureBinding2D = gl.GetInteger(GLEnum.TextureBinding2D);
@@ -77,8 +107,7 @@ public readonly struct GLState : IDisposable {
 
     Span<int> polyMode = stackalloc int[2];
     gl.GetInteger(GLEnum.PolygonMode, polyMode);
-    frontPolygonMode = polyMode[0];
-    backPolygonMode = polyMode[1];
+    frontAndBackPolygonMode = polyMode[0];
   }
 
   /// <summary>
@@ -131,10 +160,8 @@ public readonly struct GLState : IDisposable {
       (BlendingFactor)blendDstAlpha);
     gl.CheckError("GLState restore BlendFunc");
 
-    gl.PolygonMode(TriangleFace.Front, (PolygonMode)frontPolygonMode);
-    gl.CheckError("GLState restore PolygonMode Front");
-    gl.PolygonMode(TriangleFace.Back, (PolygonMode)backPolygonMode);
-    gl.CheckError("GLState restore PolygonMode Back");
+    gl.PolygonMode(TriangleFace.FrontAndBack, (PolygonMode)frontAndBackPolygonMode);
+    gl.CheckError("GLState restore PolygonMode");
 
     gl.Scissor(scissorBoxX, scissorBoxY, scissorBoxWidth, scissorBoxHeight);
     gl.CheckError("GLState restore ScissorBox");
