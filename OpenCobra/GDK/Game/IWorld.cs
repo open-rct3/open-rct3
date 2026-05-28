4d089b28896e5b5c44c1d3376beda6a9b1be5465
Chance Snow <git@chancesnow.me>
2026-05-28 16:47:00 -0500

    Attach and Stop systems automatically

--- OpenCobra/GDK/Game/IWorld.cs
+++ OpenCobra/GDK/Game/IWorld.cs
@@ -5,11 +5,14 @@
 //
 // Copyright © 2026 OpenRCT3 Contributors. All rights reserved.
 
+using DryIoc;
 using OpenCobra.GDK.Streaming;
 
 namespace OpenCobra.GDK.Game;
 
 public interface IWorld : IDisposable {
+  public readonly static Container IoC = new();
+
   /// <summary>
   /// The current progress of the world loading.
   /// </summary>
@@ -21,33 +24,4 @@ public interface IWorld : IDisposable {
   IReadOnlyCollection<ISystem> Systems { get; }
 
   void Load();
-}
-
-public abstract class World : IWorld {
-  private readonly IList<ISystem> systems = [];
-  private bool disposed;
-
-  public Progress Progress { get; protected set; } = Progress.COMPLETE;
-  public IReadOnlyCollection<ISystem> Systems => systems.AsReadOnly();
-
-  public abstract void Load();
-
-  protected virtual void Dispose(bool disposing) {
-    if (disposed) return;
-    if (disposing) {
-      // Shutdown all systems
-      foreach (var system in systems) {
-        system.Stop();
-        system.Dispose();
-      }
-      systems.Clear();
-    }
-    disposed = true;
-  }
-
-  public void Dispose() {
-    // Do not change this code! Put cleanup code in 'Dispose(bool disposing)' method
-    Dispose(disposing: true);
-    GC.SuppressFinalize(this);
-  }
 }
