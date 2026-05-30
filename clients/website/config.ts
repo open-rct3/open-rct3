2af159ea5ec01330f4806bfe7068f0633f336f45
Chance Snow <git@chancesnow.me>
2026-05-30 18:05:54 -0500

    Fix the website base URL

--- clients/website/config.ts
+++ clients/website/config.ts
@@ -14,7 +14,7 @@ const site = lume({
 // Static data
 site.data("siteTitle", "OpenRCT3");
 site.data("copyright", `2024-${new Date().getFullYear()}`);
-site.data("baseUrl", isGitHubActions ? "https://open-rct3.github.io/open-rct3" : "/");
+site.data("baseUrl", isGitHubActions ? "https://open-rct3.github.io/open-rct3/" : "/");
 site.data("forumUrl", "https://github.com/open-rct3/open-rct3/discussions");
 site.data("wikiUrl", "https://github.com/open-rct3/open-rct3/wiki");
 
