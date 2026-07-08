33a0faaaaa6211823098754e659a82167ddf2aa4
Chance Snow <git@chancesnow.me>
2026-07-08 04:03:21 -0500

    Fix button sizes and disable window resizing

--- OpenRCT3/Scenario/Editor.cs
+++ OpenRCT3/Scenario/Editor.cs
@@ -7,17 +7,22 @@
 
 using Hexa.NET.ImGui;
 using OpenCobra.GDK.GUI;
+using System.Numerics;
 
 namespace OpenRCT3.Scenario;
 
 public class Editor : IWindow {
+  // Wide enough to fit the "Choose Objectives & Challenges" button's label.
+  private const float ButtonWidth = 235;
+
   public bool Open { get; private set; } = true;
 
   public void Render() {
     if (!Open) return;
 
     var open = Open;
-    ImGui.Begin("Scenario Editor", ref open);
+    ImGui.SetNextWindowSize(new Vector2(ButtonWidth + ImGui.GetStyle().WindowPadding.X * 2, 0), ImGuiCond.Once);
+    ImGui.Begin("Scenario Editor", ref open, ImGuiWindowFlags.NoResize);
 
     // Row of icon buttons
     if (ImGui.Button("Save")) {
@@ -26,18 +31,19 @@ public class Editor : IWindow {
     ImGui.SameLine();
     if (ImGui.Button("Quit")) {
       open = false;
+      // TODO: Quit the game
     }
 
     ImGui.Separator();
 
     // Column of labeled buttons
-    if (ImGui.Button("Setup Park", new System.Numerics.Vector2(200, 0))) {
+    if (ImGui.Button("Setup Park", new Vector2(ButtonWidth, 0))) {
       // TODO: Open park dialog
     }
-    if (ImGui.Button("Choose Finances", new System.Numerics.Vector2(200, 0))) {
+    if (ImGui.Button("Choose Finances", new Vector2(ButtonWidth, 0))) {
       // TODO: Open finances dialog
     }
-    if (ImGui.Button("Choose Objectives & Challenges", new System.Numerics.Vector2(200, 0))) {
+    if (ImGui.Button("Choose Objectives & Challenges", new Vector2(ButtonWidth, 0))) {
       // TODO: Open objectives dialog
     }
 
